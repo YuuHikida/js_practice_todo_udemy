b8e30c0c78afa135e017f561f877a328909ef476
YuuuDays <144862458+YuuHikida@users.noreply.github.com>
2024-12-03 21:54:28 +0000

    add button

--- src/index.mjs
+++ src/index.mjs
@@ -18,13 +18,28 @@ const onClickAdd = (element) => {
   p.className = "todo-item";
   p.innerText = inputText;
 
+  //button(完了)createElement
+  const compleatButton = document.createElement("button");
+  compleatButton.innerText = "完了";
+  compleatButton.addEventListener("click", () => {
+    alert();
+  });
+
+  //button(削除)
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert();
+  });
+
   // 階層構造の実現
   div.appendChild(p);
+  div.appendChild(compleatButton);
+  div.appendChild(deleteButton);
   li.appendChild(div);
-
-  console.log(li);
-
   document.getElementById("incomplete-list").appendChild(li);
+
+  console.log(compleatButton);
 };
 
 // addEventListener <-イベントを読み取る
