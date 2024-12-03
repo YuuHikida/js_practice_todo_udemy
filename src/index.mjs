ddccc0af91c6d6c3f09ed1993b4ed74ebc10a11f
YuuuDays <144862458+YuuHikida@users.noreply.github.com>
2024-12-03 13:37:11 +0000

    ref

--- src/index.mjs
+++ src/index.mjs
@@ -1,1 +1,16 @@
 import "./styles.css";
+
+const onClickAdd = (element) => {
+  //alert();
+  const inputText = document.getElementById("add-text").value;
+  //textbotを空にする
+  document.getElementById("add-text").value = "";
+
+  //li生成
+  const li = document.createElement("li");
+  console.log("----");
+  console.log(li);
+};
+
+// addEventListener <-イベントを読み取る
+document.getElementById("add-button").addEventListener("click", onClickAdd);
