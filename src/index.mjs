7a89fbc4dc59e35597b86666991947a2d2463abc
YuuuDays <144862458+YuuHikida@users.noreply.github.com>
2024-12-03 14:04:25 +0000

    chapter36 done

--- src/index.mjs
+++ src/index.mjs
@@ -1,15 +1,30 @@
 import "./styles.css";
 
 const onClickAdd = (element) => {
-  //alert();
+  //  alert();
   const inputText = document.getElementById("add-text").value;
   //textbotを空にする
   document.getElementById("add-text").value = "";
 
   //li生成
   const li = document.createElement("li");
-  console.log("----");
+
+  //div
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // /p tag
+  const p = document.createElement("p");
+  p.className = "todo-item";
+  p.innerText = inputText;
+
+  // 階層構造の実現
+  div.appendChild(p);
+  li.appendChild(div);
+
   console.log(li);
+
+  document.getElementById("incomplete-list").appendChild(li);
 };
 
 // addEventListener <-イベントを読み取る
